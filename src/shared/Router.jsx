f637da1eb18083dfe77b89f3dabee8ab3114d536
jinsil <wlstlf9315@gmail.com>
2024-11-16 02:01:08 +0900

    페이지 이름 변경

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Auth from "../pages/Auth";
-import LogIn from "../pages/LogIn";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SignIn from '../pages/SignIn';
+import LogIn from '../pages/LogIn';
 
 const Router = () => {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LogIn />} />
-          <Route path="/auth" element={<Auth />} />
-        </Routes>
-      </BrowserRouter>
-    );
-  };
-  
-  export default Router;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LogIn />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
