8b6f2b6ed958c1e3be00f2fbdf50a9f09c3c7725
jinsil <wlstlf9315@gmail.com>
2024-11-17 20:00:10 +0900

    signup branch first commit

--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -1,8 +1,35 @@
+import { Button } from '../../style/buttonStyle';
+import { AuthDiv, Title } from '../../style/logIn/logInStyle';
+import { MdLockOutline, MdOutlineFaceUnlock, MdOutlineLockPerson, MdOutlineMail } from 'react-icons/md';
+import { FormDiv2 } from '../../style/logIn/signUpStyle';
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
-  )
-}
+    <AuthDiv>
+      <Title>WOD</Title>
+      <FormDiv2>
+        <p>오운완 커뮤니티에 오신 것을 환영합니다</p>
+        <form>
+          <label htmlFor="name">
+            <MdOutlineFaceUnlock className="icon" /> <input type="text" id="name" placeholder="닉네임" />
+          </label>
+          <label htmlFor="email">
+            <MdOutlineMail className="icon" />
+            <input type="email" id="email" placeholder="이메일 주소" />
+          </label>
+          <label htmlFor="pw">
+            <MdLockOutline className="icon" />
+            <input type="password" id="pw" placeholder="8~12자리의 비밀번호" />
+          </label>
+          <label htmlFor="checkPw">
+            <MdOutlineLockPerson className="icon" />
+            <input type="password" id="checkPw" placeholder="비밀번호 확인" />
+          </label>
+          <Button>계정 만들기</Button>
+        </form>
+      </FormDiv2>
+    </AuthDiv>
+  );
+};
 
-export default SignUp
+export default SignUp;
