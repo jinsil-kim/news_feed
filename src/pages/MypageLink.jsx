30a026b178e8d4bc5554664bab6898a4b2a10947
jamscreater <herlegangs2030@gmail.com>
2024-11-18 18:15:08 +0900

    Add files via upload

---
+++ src/pages/MypageLink.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Mypage from '../components/userData/Mypage'
+
+const Mypagelink = () => {
+  return (
+   <Mypage/>
+  )
+}
+
+export default Mypagelink
---
+++ src/pages/UpdateProfilePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import UpdateProfile from '../components/userData/UpdateProfile'
+
+const UpdateProfilepage = () => {
+  return (
+    <UpdateProfile/>
+  )
+}
+
+export default UpdateProfilepage
