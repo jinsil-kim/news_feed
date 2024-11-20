03a8298375c875973cdada396ff7673bc470ea7c
jinsil <wlstlf9315@gmail.com>
2024-11-20 21:13:45 +0900

    home component style change

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,30 +1,25 @@
 import { useState, useEffect } from 'react';
-import { PostHomeDiv, PostContentDiv, PostFeedDiv } from '../../style/Home/homeStyle';
-import styled from 'styled-components';
+import {
+  PostHomeDiv,
+  PostContentDiv,
+  PostFeedDiv,
+  Content,
+  Tags,
+  User,
+  ProfileImg,
+  Nickname
+} from '../../style/Home/homeStyle';
 import { supabase } from '../../supabase/supabaseClient';
-
-const Content = styled.p`
-  margin-left: 125px;
-`;
-
-const Tags = styled.div`
-  margin-left: 125px;
-`;
-
 const Home = () => {
   const [posts, setPosts] = useState([]);
-
   useEffect(() => {
     const fetchPosts = async () => {
       try {
         const { data, error } = await supabase.from('posts').select('*');
-
         console.log(data);
-
         if (error) {
           throw new Error(error.message);
         }
-
         setPosts(data);
       } catch (error) {
         alert(`데이터 로드 실패: ${error.message}`);
@@ -32,7 +27,6 @@ const Home = () => {
     };
     fetchPosts();
   }, []);
-
   console.log(posts);
   return (
     <div>
@@ -47,8 +41,9 @@ const Home = () => {
           >
             <PostHomeDiv>
               <UserProfile userId={post.user_id} />
-              <Content>{post.content}</Content>
-            </PostHomeDiv>
+              <PostFeedDiv>
+                <img src={post.img_url} />
+                <Content>{post.content}</Content>
             <Tags>
               {post.tags.map((t) => (
                 <PostContentDiv key={t}>
@@ -56,60 +51,34 @@ const Home = () => {
                 </PostContentDiv>
               ))}
             </Tags>
-            <PostFeedDiv>
-              <img src={post.img_url} />
-            </PostFeedDiv>
+              </PostFeedDiv>
+            </PostHomeDiv>
           </li>
         ))}
       </ul>
     </div>
   );
 };
-
 export default Home;
-
-const User = styled.div`
-  display: flex;
-
-  align-items: center;
-
-  gap: 24px;
-`;
-const ProfileImg = styled.img`
-  width: 100px;
-  height: 100px;
-
-  border-radius: 50%;
-`;
-
-const Nickname = styled.h3`
-  font-size: 25px;
-`;
-
 function UserProfile({ userId }) {
   const [nickName, setNickName] = useState('');
   const [profileImage, setProfileImage] = useState('');
-
   useEffect(() => {
     const fetchUser = async () => {
       const { data, error } = await supabase.from('users').select('nickname, profile_img').eq('id', userId);
-
       if (error) {
         console.error('Error fetching user data:', error);
         return;
       }
-
       if (data.length > 0) {
         setNickName(data[0].nickname);
         setProfileImage(data[0].profile_img);
       }
     };
-
     if (userId) {
       fetchUser();
     }
   }, [userId]);
-
   return (
     <User>
       <ProfileImg src={`${profileImage}`} alt="Profile" />
--- src/style/Home/homeStyle.js
+++ src/style/Home/homeStyle.js
@@ -1,18 +1,15 @@
 import styled from 'styled-components';
-
 export const Section = styled.section`
   left: 450px;
   position: relative;
   margin: 50px 0 80px 0;
   max-width: 1280px;
 `;
-
 export const PostHomeDiv = styled.div`
   padding: 20px;
   background-color: #fff;
   margin-bottom: 40px;
 `;
-
 export const PostFeedDiv = styled.div`
   margin-left: 125px;
   img {
@@ -24,7 +21,6 @@ export const PostFeedDiv = styled.div`
     box-shadow: rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, rgba(9, 30, 66, 0.08) 0px 0px 0px 1px;
   }
 `;
-
 export const PostContentDiv = styled.div`
   display: inline-block;
   background-color: #f1f1f1;
@@ -35,4 +31,24 @@ export const PostContentDiv = styled.div`
   font-size: 14px;
   color: #555;
   transition: background-color 0.3s ease;
+`;
+export const Content = styled.p`
+  margin-top: 30px;
+  font-size: 25px;
+`;
+export const Tags = styled.div`
+  margin-top: 30px;
+`;
+export const User = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 24px;
+`;
+export const ProfileImg = styled.img`
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+`;
+export const Nickname = styled.h3`
+  font-size: 25px;
 `;
