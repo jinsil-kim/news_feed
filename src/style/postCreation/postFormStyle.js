dc683b3717a9a136c6946b70951fd4570151213a
ovezen <klimen1005@gmail.com>
2024-11-20 12:56:17 +0900

    README.md 공유 기능 설명 삭제

--- src/style/postCreation/postFormStyle.js
+++ src/style/postCreation/postFormStyle.js
@@ -2,15 +2,15 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
-  max-width: 630px; /* 다른 컴포넌트들과 동일한 max-width 설정 */
+  max-width: 630px;
   margin-left: auto;
-  margin-right: auto; /* 중앙 정렬 */
+  margin-right: auto; 
   padding: 20px;
   box-sizing: border-box;
 
-  display: flex; /* Flexbox 사용 */
-  flex-direction: column; /* 요소들을 세로로 배치 */
-  align-items: center; /* 수평 중앙 정렬 */
-  justify-content: center; /* 수직 중앙 정렬 */
-  min-height: 100vh; /* 전체 화면 중앙에 위치하도록 높이 설정 */
+  display: flex;
+  flex-direction: column; 
+  align-items: center; 
+  justify-content: center;
+  min-height: 100vh;
 `;
